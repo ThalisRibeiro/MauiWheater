1e96b49e42c8769c509d80f8e122e38831321a4e
Thalis Ribeiro <thalis199@outlook.com>
2022-12-07 17:26:25 -0300

    Classe que converte content da api para objetos

---
+++ Services/Converter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MauiWheater.Models;
+using Newtonsoft.Json;
+namespace MauiWheater.Services
+{
+    public class Converter
+    {
+        public List<SearchedCity> ToSearchedCity(string json)
+        {
+            try
+            {
+                var ListOfCities = JsonConvert.DeserializeObject<List<SearchedCity>>(json);
+                return ListOfCities;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            
+        }
+    }
+}
