49f725d9d5b0ff6311d9f20c96a177704e9a8f3b
Thalis Ribeiro <thalis199@outlook.com>
2022-12-07 17:27:35 -0300

    Classe relacionada ao autocomplete da api

---
+++ Models/SearchedCity.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiWheater.Models
+{
+    public class SearchedCity
+    {
+        [JsonProperty("LocalizedName")]
+        required public string CityName { get; set; }
+        [JsonProperty("Key")]
+        required public string CityKey { get; set; }
+
+        [JsonProperty("Country")]
+        required public Country Country { get; set; }
+
+        override public string ToString()
+        {
+            return ($"{CityName}, {Country.CountryId}");
+        }
+    }
+    public class Country
+    {
+        [JsonProperty("LocalizedName")]
+        required public string CountryName { get; set; }
+        [JsonProperty("ID")]
+        required public string CountryId { get; set; }
+    }
+}
