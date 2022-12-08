a81a90cf559721b869a55f33bf09053fd0600f6c
Thalis Ribeiro <thalis199@outlook.com>
2022-12-08 17:46:18 -0300

    cirando a viewmodel e inserindo o resultado da busca

---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,55 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using MauiWheater.Models;
+using MauiWheater.Services;
+using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiWheater.ViewModels
+{
+    [INotifyPropertyChanged]
+    public partial class MainViewModel
+    {
+        public MainViewModel()
+        {
+            SearchedCities= new ObservableCollection<SearchedCity>();
+        }
+        [ObservableProperty]
+        string searchString;
+        [ObservableProperty]
+        string apiKey;
+        [ObservableProperty]
+        public ObservableCollection<SearchedCity> searchedCities;
+
+        [RelayCommand]
+        public void SearchCity()
+        {
+            //searchedCities= new ObservableCollection<SearchedCity>();   
+            Api.ApiKey = apiKey;
+            var result = Api.SearchCity(searchString);
+            var json = Api.JsonContent;
+            Converter cv = new Converter();
+            //searchedCities.AddRange(cv.ToSearchedCity(json));
+            adding(cv.ToSearchedCity(json));
+            //searchedCities= cv.ToSearchedCity(json);
+        }
+        void adding(List<SearchedCity> cities)
+        {
+            for (int i = 0; i < cities.Count; i++)
+            {
+                searchedCities.Add(cities[i]);
+            }
+        }
+        bool canSearch()
+        {
+            if (apiKey != "" && searchString != "")
+                return true;
+            return false;
+        }
+    }
+}
