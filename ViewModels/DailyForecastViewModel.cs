4409607466447c0d2b7e5dabc667969b8a12c6a7
Thalis Ribeiro <thalis199@outlook.com>
2022-12-09 16:12:43 -0300

    criacao da page e da viewmodel de DailyForecast

---
+++ ViewModels/DailyForecastViewModel.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static MauiWheater.Models.CityForecast;
+
+namespace MauiWheater.ViewModels
+{
+    [ObservableObject]
+    public partial class DailyForecastViewModel
+    {
+        public DailyForecastViewModel() { }
+
+        [ObservableProperty]
+        Root root;
+    }
+}
---
+++ Views/DailyForecastPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiWheater.Views;
+
+public partial class DailyForecastPage : ContentPage
+{
+	public DailyForecastPage()
+	{
+		InitializeComponent();
+	}
+}
